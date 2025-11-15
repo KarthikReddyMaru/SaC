9ae0e5b55c70ab62ccf701115c667be2bcfdcfa7
Maru Karthik Reddy <karthikreddy.maru@gmail.com>
2025-11-16 01:14:42 +0530

    initial commit

---
+++ src/main/java/com/sac/SaCApplication.java
@@ -0,0 +1,13 @@
+package com.sac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaCApplication.class, args);
+    }
+
+}
